3fbb4daf63a951a2c4de6ea265d9947b4e09d92e
lalaarukh <lrukh@smith.edu>
2023-02-15 13:40:09 -0500

    uploading chatbot (without ifelse)

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,44 @@
-class Conversation {
+import java.util.Scanner;
+import java.util.Random;
 
-  public static void main(String[] arguments) {
-    // You will start the conversation here.
+class Conversation {
+  public static void main(String[] arguments) {  
+
+    System.out.println("How many rounds?");
+    Scanner round_count = new Scanner(System.in); 
+    Integer r = round_count.nextInt();
+    round_count.close();
+    System.out.println("Hello there! What's on your mind?");
+  
+    String[] canned_responses = new String[] {"I know right!", "Tell me about it", "Oh my God me too"}; 
+    Random random = new Random();
+    Scanner in = new Scanner(System.in);
+    String [] toMirror = {"I", "me", "am","you","my","your"};
+    String [] replacements = {"you","you","are","I","your","my"};
+
+    for (int i = 0; i < r; i++){ 
+      String input = in.nextLine();
+      //String input.nextLine();
+      //System.out.println(input);
+      String [] words = input.split(" ");
+      String newSentence = "";
+      for (int j = 0; j < words.length; j++){
+        String l = words[j];
+        for (int m = 0; m < toMirror.length; m++ ){
+          if (words[j].equals(toMirror[j])){
+            l = replacements[j]; 
+          }
+        }
+        if (l.equals(" ")){;
+          l = words[j];
+      }
+      newSentence += " " + l;
+    }
+      String response = canned_responses[random.nextInt(canned_responses.length)];
+      System.out.println(response);
+    } 
+
+    System.out.println("See ya!");
+    in.close();
   }
 }
--- reflection.md
+++ reflection.md
@@ -1,7 +1,9 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+Fatima and Musa
 - Which session(s) of TA / office hours did you attend?
+Sunday 1-3 with Chris
 - What are your initial impressions of Java? 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
